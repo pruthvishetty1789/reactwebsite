75c0cebb1ecd7457e7ed4b2ba615337d156f6540
Pruthvi <146100300+pruthvishetty1789@users.noreply.github.com>
2024-11-04 23:01:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/header';
+import Home from './components/Home/Home';
+function App() {
+  return (
+    <div className="App">
+     <h2>djf</h2> 
+     <Header/>
+     <Home/> 
+    </div>
+  );
+}
+
+export default App;
